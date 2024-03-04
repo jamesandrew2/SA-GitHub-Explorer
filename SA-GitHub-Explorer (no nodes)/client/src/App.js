983659ceb81d2954744c8a85b26594799cad93a2
James Andrew Alison <135700129+jamesandrew2@users.noreply.github.com>
2024-03-04 09:11:14 +0200

    Add files via upload

---
+++ SA-GitHub-Explorer (no nodes)/client/src/App.js
@@ -0,0 +1,26 @@
+// Import React and necessary components from react-router-dom for navigation
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// Import your custom components that handle searching and displaying user details
+import Search from './Search';
+import UserDetails from './UserDetails';
+
+function App() {
+  return (
+    // Use Router to wrap your application to enable routing
+    <Router>
+      <div>
+        {/* Switch component is used to render only the first route that matches the location */}
+        <Switch>
+          {/* Route for the search page, which will render when the path is exactly "/" */}
+          <Route path="/" exact component={Search} />
+          {/* Route for displaying details of a specific user */}
+          {/* :username is a URL parameter that UserDetails component can access */}
+          <Route path="/users/:username" component={UserDetails} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
